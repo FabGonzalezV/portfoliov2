c33223dce8f83264f039eca917fa933c64757ebc
FabGonzalezV <fabian.gonzalez.valentin@gmail.com>
2023-08-19 14:10:11 -0600

    rename rutes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ export default function App() {
             <Routes>
                 <Route path="/portfoliov2/inicio" element={<LandingPage />} />
                 <Route path="/portfoliov2/projects" element={<MainPage />} />
-
                 <Route path="/portfoliov2/about" element={<About />} />
                 <Route path="/portfoliov2/contact" element={<ContactForm />} />
             </Routes>
